90992b8e01b5c01895cb236e5e3d30d35d2f79a6
aapowadi <aapowadi@gmail.com>
2021-07-03 15:39:02 -0500

    Update

--- test_list.py
+++ test_list.py
@@ -38,7 +38,7 @@ def exs(name):
     ex_test["solver"] = Solver_reim_cm
     ex_test["model"] = Model_8n_sub1
     ex_test["batch_size"] = 128
-    ex_test["learning_rate"] = 0.005
+    ex_test["learning_rate"] = 0.001
     ex_test["num_iterations"] = 100
     ex_test["debug_output"] = True
     ex_test["log_path"] = "./log_syn/ex_test/"
